f511cc28869a3d671e93ca9a6a489dc03eef579a
laineywatlington <lainey.watlington@gmail.com>
2019-05-03 18:18:30 -0700

    Update IntegerHashes.cpp

--- IntegerHashes.cpp
+++ IntegerHashes.cpp
@@ -1,38 +1,28 @@
 #include "IntegerHashes.h"
 #include <cmath>
-using namespace std;
 
-IntegerHash::IntegerHash(uint64_t i, uint64_t m){ //initilizing stuff
-  this -> i = i;
-  this -> m = m;
+IntegerHash::IntegerHash(uint64_t i, uint64_t m): i(i), m(m) {}
+
+DivisionHash::DivisionHash(uint64_t i, uint64_t m): IntegerHash(i,m) {}
+
+uint64_t DivisionHash::hash(uint64_t input) const {
+	uint64_t key = input * (i + 1);
+	return key % m;
 }
 
-DivisionHash::DivisionHash(uint64_t i, uint64_t m) : IntegerHash(i, m){ //initializing stuff... can i do this using integerhash?
+ReciprocalHash::ReciprocalHash(uint64_t i, uint64_t m): IntegerHash(i,m) {
+	b = 1.0 / (i + 2);
 }
 
-// This function uses the "division method" of hashing
-// It first multiplies the input by i + 1, then reduces the result modulo m
-uint64_t DivisionHash::hash(uint64_t input) const{
-  uint64_t newinput = input * (i + 1);
-  return newinput % m;
+
+uint64_t ReciprocalHash::hash(uint64_t input) const {
+	return static_cast<uint64_t>(m * ((input * b) - static_cast<uint64_t>(input * b)));
 }
 
-// This function uses the "multiplication method" of hashing
-// It multiplies the input by 1 / (i + 2), then multiplies the fractional part by m
-ReciprocalHash::ReciprocalHash(uint64_t i, uint64_t m) : IntegerHash(i, m){ //let integerhash initialize everything so i dont have to keep doing it
-  b = 1 / (i + 2);
+SquareRootHash::SquareRootHash(uint64_t i, uint64_t m): IntegerHash(i,m) {
+	b = std::sqrt(4*i + 3);
 }
 
-uint64_t ReciprocalHash::hash(uint64_t input) const{
-  return static_cast<uint64_t>(m * ((input * b) - static_cast<uint64_t>(input * b)));
-}
-
-// This function also uses the "multiplication method" of hashing
-// It multiplies the input by the square root of 4i + 3, then multiplies the fractional part by m
-SquareRootHash::SquareRootHash(uint64_t i, uint64_t m) : IntegerHash(i, m){
-  b = sqrt(4 * i + 3);
-}
-
-uint64_t SquareRootHash::hash(uint64_t input) const{
- return static_cast<uint64_t>(m * ((input * b) - static_cast<uint64_t>(input * b))); 
+uint64_t SquareRootHash::hash(uint64_t input) const {
+	return static_cast<uint64_t>(m * ((input * b) - static_cast<uint64_t>(input * b)));
 }
