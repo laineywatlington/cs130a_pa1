9f7b09d7c3f98139318d18e5d66c7fd83db610e3
laineywatlington <lainey.watlington@gmail.com>
2019-05-03 17:23:46 -0700

    jenkins possibly done

--- StringHashes.cpp
+++ StringHashes.cpp
@@ -1,1 +1,16 @@
 #include "StringHashes.h"
+
+uint64_t JenkinsHash::hash(const std::string& input) const{
+  uint32_t hash = 0;
+  size_t length = input.length();
+  size_t i;
+  for(i = 0; i < length; i++){
+    hash += input[i];
+    hash += (hash << 10);
+    hash ^= (hash >> 6);
+  }
+  hash += (hash << 3);
+  hash ^= (hash >> 11);
+  hash += (hash << 15);
+  return hash;
+}
