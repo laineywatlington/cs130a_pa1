f8cdbc3f1ee13593786a624723f8ae1732009723
laineywatlington <lainey.watlington@gmail.com>
2019-05-04 12:17:19 -0700

    hashset maybe done

--- HashSet.cpp
+++ HashSet.cpp
@@ -1,1 +1,14 @@
 #include "HashSet.h"
+
+using namespace std
+
+HashSet::HashSet(){
+  nitems = 0;
+  nslots = 100000;
+  intfn = new SquareRootHash(5, nslots);
+  strfn = new JenkinsHash;
+  slots = new string * [nslots];
+  for(i = 0; i < nslots; i++){
+    slots[i] = nullptr;
+  }
+}
