6afccfa555a326daafa2f11843e702e43461a3ff
laineywatlington <lainey.watlington@gmail.com>
2019-05-07 13:53:12 -0700

    Update BloomFilter.cpp

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -46,7 +46,7 @@ void BloomFilter::insert(const std::string& value){
 
 bool BloomFilter::lookup(const std::string& value) const{
 	for (int i = 0; i < k; ++i) {
-		uint64_t newValue = strfn -> hash(value)
+		uint64_t newValue = strfn -> hash(value);
 		uint64_t location = intfns[i] -> hash(newValue);
 		if (!(bits[location / 64] & (static_cast<uint64_t>(1) << (location % 64)))) {
 			return false;
