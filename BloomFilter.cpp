b316b6aa115a87731b297f6c7dd69c916640751e
laineywatlington <lainey.watlington@gmail.com>
2019-05-07 13:51:22 -0700

    Update BloomFilter.cpp

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -1,7 +1,7 @@
 #include "IntegerHashes.h"
 #include "StringHashes.h"
 #include "BloomFilter.h"
-#include <iostream>
+
 using namespace std; 
 
 BloomFilter::BloomFilter(int k, int m, std::string strfn, std::string intfn){
@@ -45,15 +45,14 @@ void BloomFilter::insert(const std::string& value){
 }
 
 bool BloomFilter::lookup(const std::string& value) const{
- 	for(int i = 0; i < k; i++){
-		uint64_t newValue = strfn -> hash(value); 
+	for (int i = 0; i < k; ++i) {
+		uint64_t newValue = strfn -> hash(value)
 		uint64_t location = intfns[i] -> hash(newValue);
-		uint64_t temp = bits[location / 64] & (uint64_t(1) << (location % 64)); 
-		if(!(temp)){  
+		if (!(bits[hash/64] & (static_cast<uint64_t>(1) << (hash % 64)))) {
 			return false;
-		} 
+		}
 	}
-	return true; 
+	return true;
 }
 
 //destructor
