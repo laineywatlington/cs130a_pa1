77cbc11abd9fc3be9cbd4974ca5e3feb53249ca4
laineywatlington <lainey.watlington@gmail.com>
2019-05-07 15:02:35 -0700

    Update BloomFilter.cpp

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -36,7 +36,7 @@ BloomFilter::BloomFilter(int k, int m, std::string strfn, std::string intfn){
 
 }
 
-/*void BloomFilter::insert(const std::string& value){
+void BloomFilter::insert(const std::string& value){
 	for(int i = 0; i < k; i++){
 		uint64_t newValue = strfn -> hash(value);
 		uint64_t location = intfns[i] -> hash(newValue); 
@@ -54,26 +54,8 @@ bool BloomFilter::lookup(const std::string& value) const{
 	}
 	return true;
 }
-*/
 
-void BloomFilter::insert(const std::string& value){
-	for(int i = 0; i < k; i++){
-		uint64_t newValue = strfn -> hash(value);
-		uint64_t location = intfns[i] -> hash(newValue); 
-		bits[location / 64] |= (uint64_t(1) << (location % 64)); 
-	}
-}
-bool BloomFilter::lookup(const std::string& value) const{
- 	for(int i = 0; i < k; i++){
-		uint64_t newValue = strfn -> hash(value); 
-		uint64_t location = intfns[i] -> hash(newValue);
-		uint64_t temp = bits[location / 64] & (uint64_t(1) << (location % 64)); 
-		if(!(temp)){  
-			return false;
-		} 
-	}
-	return true; 
-}
+
 //destructor
 BloomFilter::~BloomFilter(){
 	delete [] bits;
