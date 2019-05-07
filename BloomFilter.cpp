aa35bfb6dc6ad7f0e074ff9025677d599e8e18b4
laineywatlington <lainey.watlington@gmail.com>
2019-05-06 19:06:10 -0700

    Update BloomFilter.cpp

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -1,8 +1,37 @@
 #include "BloomFilter.h"
 
-//implement BloomFilter function
+//bloom filter constructor
 BloomFilter::BloomFilter(int k, int m, std::string strfn, std::string intfn){
-  
+  this -> k = k;
+  this -> m = m;
+  this -> bits = new uint64_t[m];
+  //check which kind of hash it is and respon accordingly
+  if(strfn == "jenkins"){
+    this -> strfn = new JenkinsHash();
+  }
+  else if(strfn == "pearson"){
+    this -> strfn = new PearsonHash();
+  }
+  intfns = new IntegerHash*[k];
+  for(int i = 0; i < k; i++){
+    if(intfn == "division"){
+      this -> intfns[i] = new DivisionHash(i, m);
+    }
+    else if(intfn == "reciprocal"){
+      this -> intfns[i] = new ReciprocalHash(i, m);
+    }
+    else if(intfn == "squareroot"){
+      this -> intfns[i] = new SquareRootHash(i, m);
+    }
+  }
 }
+
 //destructor
-BloomFilter::~BloomFilter()
+BloomFilter::~BloomFilter(){
+  for(int i = 0; i < k; i++){
+    delete intfns[i];
+  }
+  delete [] intfns;
+  delete strfn;
+  delete [] bits;
+}
